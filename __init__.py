9742badc43abb014fc62188378c0b3c6ed3dcc69
shelton1007 <72802575+shelton1007@users.noreply.github.com>
2024-07-18 15:37:19 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -77,11 +77,11 @@ def enregistrer_client():
     conn.close()
     return redirect('/consultation/')  # Rediriger vers la page d'accueil après l'enregistrement
 
-    @app.route('/fiche_nom/<post_id>')
-    def Readfiche_2(post_id):
+@app.route('/fiche_nom/<string:post_id>')
+def Readfiche_2(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_id,))
+    cursor.execute('SELECT * FROM clients WHERE nom like ?', (post_id,))
     data = cursor.fetchall()
     conn.close()
     # Rendre le template HTML et transmettre les données
